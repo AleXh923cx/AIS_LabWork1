c554d0ef158e03f197e36ed17b7a988b8ec68cb3
alexei4235 <lolprprvasiliev@yandex.ru>
2026-09-19 20:22:28 +0700

    fix: теперь Level по логике базы данных - вычисляемое свойство. add: свойство _age (приватное поле) и Age (публичное поле), одного приватного делящий на 10, от которого зависит Level. delete: удалил приватное поле _level.

--- Model/Character.cs
+++ Model/Character.cs
@@ -2,12 +2,14 @@
 {
     public class Character
     {
-        private int _level;
-        public int Level { 
-            get
-            {
-                return _level;
-            } 
+        private int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
         }
+
+        public int Level => _age / 10;
     }
 }
