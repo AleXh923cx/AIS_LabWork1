994242dd36c58fb4879e36583dd81967f06e5980
alexei4235 <lolprprvasiliev@yandex.ru>
2026-09-19 20:13:26 +0700

    Переименовываю класс в Character. Далее, добавляю одно свойства _level приватного поля и ещё одно публичного поля Level только для чтение. (пока что невозможно изменить, только получать)

---
+++ Model/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace Model
+{
+    public class Character
+    {
+        private int _level;
+        public int Level { 
+            get
+            {
+                return _level;
+            } 
+        }
+    }
+}
