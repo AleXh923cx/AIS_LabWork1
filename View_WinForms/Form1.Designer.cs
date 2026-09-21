e8d22b9a753ebeae013189d87cb2e53b76ce0de7
alexei4235 <lolprprvasiliev@yandex.ru>
2026-09-21 19:17:51 +0700

    add: + DataGridView (таблица, составленных из атрибутов Character) + Два DataSource элементы из библиотеки Model - Character и Logic

--- View_WinForms/Form1.Designer.cs
+++ View_WinForms/Form1.Designer.cs
@@ -28,12 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             textBox_charName = new TextBox();
             label1 = new Label();
             label2 = new Label();
             textBox_charAge = new TextBox();
             button_nxt = new Button();
             button_prvs = new Button();
+            dataGridView1 = new DataGridView();
+            nameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            ageDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            levelDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            characterBindingSource = new BindingSource(components);
+            logicBindingSource = new BindingSource(components);
+            logicBindingSource1 = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)characterBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)logicBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)logicBindingSource1).BeginInit();
             SuspendLayout();
             // 
             // textBox_charName
@@ -76,7 +88,7 @@
             // 
             // button_nxt
             // 
-            button_nxt.Location = new Point(183, 135);
+            button_nxt.Location = new Point(352, 269);
             button_nxt.Name = "button_nxt";
             button_nxt.Size = new Size(100, 30);
             button_nxt.TabIndex = 4;
@@ -85,18 +97,62 @@
             // 
             // button_prvs
             // 
-            button_prvs.Location = new Point(58, 135);
+            button_prvs.Location = new Point(12, 269);
             button_prvs.Name = "button_prvs";
             button_prvs.Size = new Size(100, 30);
             button_prvs.TabIndex = 5;
             button_prvs.Text = "Предыдущий";
             button_prvs.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { nameDataGridViewTextBoxColumn, ageDataGridViewTextBoxColumn, levelDataGridViewTextBoxColumn });
+            dataGridView1.DataBindings.Add(new Binding("DataContext", logicBindingSource1, "", true));
+            dataGridView1.DataSource = characterBindingSource;
+            dataGridView1.Location = new Point(12, 113);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(440, 150);
+            dataGridView1.TabIndex = 6;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // ageDataGridViewTextBoxColumn
+            // 
+            ageDataGridViewTextBoxColumn.DataPropertyName = "Age";
+            ageDataGridViewTextBoxColumn.HeaderText = "Age";
+            ageDataGridViewTextBoxColumn.Name = "ageDataGridViewTextBoxColumn";
+            // 
+            // levelDataGridViewTextBoxColumn
+            // 
+            levelDataGridViewTextBoxColumn.DataPropertyName = "Level";
+            levelDataGridViewTextBoxColumn.HeaderText = "Level";
+            levelDataGridViewTextBoxColumn.Name = "levelDataGridViewTextBoxColumn";
+            levelDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // characterBindingSource
+            // 
+            characterBindingSource.DataSource = typeof(Model.Character);
+            // 
+            // logicBindingSource
+            // 
+            logicBindingSource.DataSource = typeof(Model.Logic);
+            // 
+            // logicBindingSource1
+            // 
+            logicBindingSource1.DataSource = typeof(Model.Logic);
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(344, 201);
+            ClientSize = new Size(464, 311);
+            Controls.Add(dataGridView1);
             Controls.Add(button_prvs);
             Controls.Add(button_nxt);
             Controls.Add(label2);
@@ -107,6 +163,10 @@
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "АИС \"РПГ персонажи-деревьи\"";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)characterBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)logicBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)logicBindingSource1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -119,5 +179,12 @@
         private TextBox textBox_charAge;
         private Button button_nxt;
         private Button button_prvs;
+        private DataGridView dataGridView1;
+        private BindingSource logicBindingSource;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn ageDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn levelDataGridViewTextBoxColumn;
+        private BindingSource characterBindingSource;
+        private BindingSource logicBindingSource1;
     }
 }
