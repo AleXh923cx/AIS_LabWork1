cd0245e90cc99fbfda626cc9e330708f5a26832c
alexei4235 <lolprprvasiliev@yandex.ru>
2026-09-21 21:45:52 +0700

    feature: + Реализую на консоли, пока что не в идеальном виде (я имею ввиду UI, чтобы смотрелось как таблица)

--- View_ConsoleApp/Program.cs
+++ View_ConsoleApp/Program.cs
@@ -7,7 +7,16 @@ namespace View_ConsoleApp
     {
         static void Main(string[] args)
         {
+            Logic logic = new Logic();
 
+            Console.WriteLine("|      Name      |  Age  |");
+            Console.WriteLine("|----------------|-------|");
+            foreach (var chrcter in logic.GetAllCharacters())
+            {
+                Console.WriteLine($"| {chrcter.Name} \t | {chrcter.Age} \t |");
+            }
+
+            Console.ReadLine();
         }
     }
 }
