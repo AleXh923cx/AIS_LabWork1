37eee417ea17516c1629d2f91ebc3890a26891fd
alexei4235 <lolprprvasiliev@yandex.ru>
2026-09-22 11:57:26 +0700

    add: + В консоли, главное меню с выбором пункта (добавление, удаление, выход) с проверками условиями, пока что не реализован

--- View_ConsoleApp/Program.cs
+++ View_ConsoleApp/Program.cs
@@ -9,14 +9,36 @@ namespace View_ConsoleApp
         {
             Logic logic = new Logic();
 
+            // Заголовки таблицы
             Console.WriteLine("| №  |    Имя     |   Вид    | Возраст | Уровень |");
             Console.WriteLine("|----|------------|----------|---------|---------|");
+
+            // Записи таблицы
             foreach (var chrcter in logic.GetAllCharacters())
             {
                 Console.WriteLine($"| {chrcter.Id,2} | {chrcter.Name,10} | {chrcter.Genus,8} | {chrcter.Age,7} | {chrcter.Level,7} |");
             }
 
-            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Пункты:");
+            Console.WriteLine("1. Добавление сущности");
+            Console.WriteLine("2. Удаление сущности");
+            Console.WriteLine("0. Выход\n");
+
+            Console.Write("Выберите пункт: "); string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out int choice) && choice >= 0 && choice < 3)
+            {
+                switch (choice)
+                {
+                    case 0:
+                        return; // Конец
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод");
+            }
         }
     }
 }
