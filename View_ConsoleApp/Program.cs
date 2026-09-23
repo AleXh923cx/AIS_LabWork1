f067d91ebe7ea79ccca288a6c779a6010650fafb
alexei4235 <lolprprvasiliev@yandex.ru>
2026-09-23 20:30:27 +0700

    change: в View_ConsoleApp, Таблица отделена от главного меню (переключить можно логическим переменным tableMode, если да - то ShowTable, если нет - то ShowMenu)

--- View_ConsoleApp/Program.cs
+++ View_ConsoleApp/Program.cs
@@ -5,169 +5,198 @@ namespace View_ConsoleApp
 {
     class Program
     {
+        static Logic logic = new Logic();
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("АИС \"РПГ персонажи-дерево\"");
+            Console.WriteLine();
+            Console.WriteLine("Пункты:");
+            Console.WriteLine("1. Добавление сущности");
+            Console.WriteLine("2. Удаление сущности");
+            Console.WriteLine("3. Изменение сущности");
+            Console.WriteLine("4. Просмотр таблиц");
+            Console.WriteLine("0. Выход\n");
+
+            Console.Write("Выберите пункт: ");
+        }
+
+        static void ShowTable()
+        {
+            // Заголовки таблицы
+            Console.WriteLine("| №  |    Имя     |   Вид    | Возраст | Уровень |");
+            Console.WriteLine("|----|------------|----------|---------|---------|");
+
+            // Записи таблицы
+            foreach (var chrcter in logic.GetAllCharacters())
+            {
+                Console.WriteLine($"| {chrcter.Id,2} | {chrcter.Name,10} | {chrcter.Genus,8} | {chrcter.Age,7} | {chrcter.Level,7} |");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Logic logic = new Logic();
-
             bool cycleMenu = true;
+            bool tableMode = false;
 
             while (cycleMenu)
             {
-                // Заголовки таблицы
-                Console.WriteLine("| №  |    Имя     |   Вид    | Возраст | Уровень |");
-                Console.WriteLine("|----|------------|----------|---------|---------|");
-
-                // Записи таблицы
-                foreach (var chrcter in logic.GetAllCharacters())
+                if (tableMode)
                 {
-                    Console.WriteLine($"| {chrcter.Id,2} | {chrcter.Name,10} | {chrcter.Genus,8} | {chrcter.Age,7} | {chrcter.Level,7} |");
+                    ShowTable();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Нажмите любую клавишу для выхода из режима таблицы ...");
+                    Console.ReadKey();
+
+                    tableMode = false;
+                    Console.Clear();
                 }
-
-                Console.WriteLine();
-                Console.WriteLine("Пункты:");
-                Console.WriteLine("1. Добавление сущности");
-                Console.WriteLine("2. Удаление сущности");
-                Console.WriteLine("3. Изменение сущности");
-                Console.WriteLine("0. Выход\n");
-
-                Console.Write("Выберите пункт: ");
-                if (int.TryParse(Console.ReadLine(), out int choice) && choice >= 0 && choice < 4)
+                else
                 {
-                    switch (choice)
+                    ShowMenu();
+                    if (int.TryParse(Console.ReadLine(), out int choice) && choice >= 0 && choice < 5)
                     {
-                        case 1:
-                            Console.Write("Введите имя персонажа: ");
-                            string charName = Console.ReadLine();
-                            if (string.IsNullOrEmpty(charName))
-                            {
-                                Console.WriteLine("ОШИБКА: Имя персонажа не должно быть пустым");
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            }
-                            Console.Write("Введите вид персонажа: ");
-                            string charCls = Console.ReadLine();
-                            if (string.IsNullOrEmpty(charCls))
-                            {
-                                Console.WriteLine("ОШИБКА: Вид персонажа не должно быть пустым");
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            }
-                            Console.Write("Введите возраст персонажа: ");
-                            if (int.TryParse(Console.ReadLine(), out int charAge))
-                            {
-                                if (charAge < 0)
+                        switch (choice)
+                        {
+                            case 1:
+                                Console.Write("Введите имя персонажа: ");
+                                string charName = Console.ReadLine();
+                                if (string.IsNullOrEmpty(charName))
                                 {
-                                    Console.WriteLine("ОШИБКА: Возраст должен быть выше нуля");
+                                    Console.WriteLine("ОШИБКА: Имя персонажа не должно быть пустым");
                                     Console.ReadKey();
                                     Console.Clear();
                                     break;
                                 }
-                                logic.AddCharacter(charName, charCls, charAge);
-                                Console.Clear();
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("ОШИБКА: Возраст должен быть числом");
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            }
-                        case 2:
-                            Console.Write("Введите № персонажа: ");
-                            if (int.TryParse(Console.ReadLine(), out int charId))
-                            {
-                                if (!logic.DeleteCharacter(charId))
+                                Console.Write("Введите вид персонажа: ");
+                                string charCls = Console.ReadLine();
+                                if (string.IsNullOrEmpty(charCls))
                                 {
-                                    Console.WriteLine("ОШИБКА: Введённый № персонажа не существует в таблице");
+                                    Console.WriteLine("ОШИБКА: Вид персонажа не должно быть пустым");
                                     Console.ReadKey();
                                     Console.Clear();
                                     break;
                                 }
-                                Console.Clear();
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("ОШИБКА: № персонажа должен быть числом");
-                                Console.ReadKey();
-                                Console.Clear();
-                                break;
-                            }
-                        case 3:
-                            Console.Write("Введите № персонажа: ");
-                            if (int.TryParse(Console.ReadLine(), out charId))
-                            {
-                                if (logic.GetCharacterById(charId) == null)
+                                Console.Write("Введите возраст персонажа: ");
+                                if (int.TryParse(Console.ReadLine(), out int charAge))
                                 {
-                                    Console.WriteLine("ОШИБКА: Введённый № персонажа не существует в таблице");
-                                    Console.ReadKey();
+                                    if (charAge < 0)
+                                    {
+                                        Console.WriteLine("ОШИБКА: Возраст должен быть выше нуля");
+                                        Console.ReadKey();
+                                        Console.Clear();
+                                        break;
+                                    }
+                                    logic.AddCharacter(charName, charCls, charAge);
                                     Console.Clear();
                                     break;
                                 }
                                 else
                                 {
-                                    Console.Write("Введите имя персонажа: ");
-                                    charName = Console.ReadLine();
-                                    if (string.IsNullOrEmpty(charName))
+                                    Console.WriteLine("ОШИБКА: Возраст должен быть числом");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                                }
+                            case 2:
+                                Console.Write("Введите № персонажа: ");
+                                if (int.TryParse(Console.ReadLine(), out int charId))
+                                {
+                                    if (!logic.DeleteCharacter(charId))
                                     {
-                                        Console.WriteLine("ОШИБКА: Имя персонажа не должно быть пустым");
+                                        Console.WriteLine("ОШИБКА: Введённый № персонажа не существует в таблице");
                                         Console.ReadKey();
                                         Console.Clear();
                                         break;
                                     }
-                                    Console.Write("Введите вид персонажа: ");
-                                    charCls = Console.ReadLine();
-                                    if (string.IsNullOrEmpty(charCls))
+                                    Console.Clear();
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("ОШИБКА: № персонажа должен быть числом");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                                }
+                            case 3:
+                                Console.Write("Введите № персонажа: ");
+                                if (int.TryParse(Console.ReadLine(), out charId))
+                                {
+                                    if (logic.GetCharacterById(charId) == null)
                                     {
-                                        Console.WriteLine("ОШИБКА: Вид персонажа не должно быть пустым");
+                                        Console.WriteLine("ОШИБКА: Введённый № персонажа не существует в таблице");
                                         Console.ReadKey();
-                                        Console.Clear();
-                                        break;
-                                    }
-                                    Console.Write("Введите возраст персонажа: ");
-                                    if (int.TryParse(Console.ReadLine(),out charAge))
-                                    {
-                                        if (charAge < 0)
-                                        {
-                                            Console.WriteLine("ОШИБКА: Возраст должен быть выше нуля");
-                                            Console.ReadKey();
-                                            Console.Clear();
-                                            break;
-                                        }
-
-                                        logic.UpdateCharacter(charId, charName, charCls, charAge);
                                         Console.Clear();
                                         break;
                                     }
                                     else
                                     {
-                                        Console.WriteLine("ОШИБКА: Возраст должен быть числом");
-                                        Console.ReadKey();
-                                        Console.Clear();
-                                        break;
+                                        Console.Write("Введите имя персонажа: ");
+                                        charName = Console.ReadLine();
+                                        if (string.IsNullOrEmpty(charName))
+                                        {
+                                            Console.WriteLine("ОШИБКА: Имя персонажа не должно быть пустым");
+                                            Console.ReadKey();
+                                            Console.Clear();
+                                            break;
+                                        }
+                                        Console.Write("Введите вид персонажа: ");
+                                        charCls = Console.ReadLine();
+                                        if (string.IsNullOrEmpty(charCls))
+                                        {
+                                            Console.WriteLine("ОШИБКА: Вид персонажа не должно быть пустым");
+                                            Console.ReadKey();
+                                            Console.Clear();
+                                            break;
+                                        }
+                                        Console.Write("Введите возраст персонажа: ");
+                                        if (int.TryParse(Console.ReadLine(), out charAge))
+                                        {
+                                            if (charAge < 0)
+                                            {
+                                                Console.WriteLine("ОШИБКА: Возраст должен быть выше нуля");
+                                                Console.ReadKey();
+                                                Console.Clear();
+                                                break;
+                                            }
+
+                                            logic.UpdateCharacter(charId, charName, charCls, charAge);
+                                            Console.Clear();
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("ОШИБКА: Возраст должен быть числом");
+                                            Console.ReadKey();
+                                            Console.Clear();
+                                            break;
+                                        }
                                     }
                                 }
-                            }
-                            else
-                            {
-                                Console.WriteLine("ОШИБКА: № персонажа должен быть числом");
-                                Console.ReadKey();
+                                else
+                                {
+                                    Console.WriteLine("ОШИБКА: № персонажа должен быть числом");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                    break;
+                                }
+                            case 4:
+                                tableMode = true;
                                 Console.Clear();
                                 break;
-                            }
-                        case 0:
-                            cycleMenu = false;
-                            break;
+                            case 0:
+                                cycleMenu = false;
+                                break;
+                        }
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Неверный ввод! (диапазон ввода: 0-2)");
-                    Console.ReadKey();
-                    Console.Clear();
+                    else
+                    {
+                        Console.WriteLine("Неверный ввод! (диапазон ввода: 0-4)");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
                 }
             }
         }
